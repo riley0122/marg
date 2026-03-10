6f5c8e2f52087ab8127d1841556fbe6544e1b28c
Riley <77288737+riley0122@users.noreply.github.com>
2026-03-10 23:35:54 +0100

    Create method class

---
+++ src/Method.cs
@@ -0,0 +1,30 @@
+// Marg programming language
+// Copyright (C) 2026 Riley0122
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+namespace marg
+{
+    class Method
+    {
+        private string Name;
+        private List<string> Params;
+        private CodeBlock Body;
+
+        public object Execute()
+        {
+            // TODO
+        }
+    }
+}
